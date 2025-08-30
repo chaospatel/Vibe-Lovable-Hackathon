8d88601292b2473d3939f8c9acd2be90b52e0b5c
Abhiraj singh <nayaabyt@gmail.com>
2025-08-30 13:50:18 +0530

    Update LikeButton.jsx

--- project omlet/src/components/LikeButton.jsx
+++ project omlet/src/components/LikeButton.jsx
@@ -1,7 +1,8 @@
 export default function LikeButton({ onClick, liked = false, count = 0 }) {
 return (
-<button onClick={onClick} className={`btn ${liked ? "bg-rose-600 textwhite" : "btn-outline"}`}>
+<button onClick={onClick} className={`btn ${liked ? "bg-rose-600 text-white" : "btn-outline"}`}>
 {count}
 </button>
 );
+
 }
